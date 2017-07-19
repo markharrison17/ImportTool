c0df2bc17eba7c5f4f5efd06695116136974cb2b
markharrison <mark.harrison17@nhs.net>
2017-07-19 13:32:22 +0100

    fixing lint issues

--- import.py
+++ import.py
@@ -137,10 +137,10 @@ def get_engine():
 if __name__ == '__main__':
 
     total_start_time = time.time()
-    
+
     # Get the XML data
     ods_xml_data = File_manager.get_latest_xml()
-    
+
     log.debug('Data Load Time = %s', time.strftime(
         "%H:%M:%S", time.gmtime(time.time() - total_start_time)))
 
@@ -148,10 +148,10 @@ if __name__ == '__main__':
     engine = get_engine()
 
     import_start_time = time.time()
-    
+
     # Do the import into the empty database
     ODSDBCreator(engine).create_database(ods_xml_data, test_mode)
-    
+
     log.debug('Data Processing Time = %s', time.strftime(
         "%H:%M:%S", time.gmtime(time.time() - import_start_time)))
 
@@ -159,4 +159,3 @@ if __name__ == '__main__':
         "%H:%M:%S", time.gmtime(time.time() - total_start_time)))
 
     log.info("Database import finished")
-
