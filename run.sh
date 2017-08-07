617e97441c9f68e6a394f4422edafab08d42b957
markharrison <mark.harrison17@nhs.net>
2017-08-07 15:10:50 +0100

    improvements

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-docker run -p 5432:5432 -d -v /mnt/git/ods/ImportTool:/usr/src/ImportTool -v /mnt/git/ods/data/:/usr/src/ImportTool/data ods-postgres-db-img
+#docker run -p 5432:5432 -d -v /mnt/git/ods/ImportTool:/usr/src/ImportTool -v /mnt/git/ods/data/:/usr/src/ImportTool/data ods-postgres-db-img
 
 #RUN IN CONTAINER:
 #psql -U postgres -c "CREATE DATABASE openods OWNER postgres;"
 #cd /usr/src/ImportTool
 #python3 import.py -d postgres -l -c postgresql://postgres@localhost/openods --verbose
+
+ls -al
