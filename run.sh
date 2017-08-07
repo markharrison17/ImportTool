d83a676108f27fcd24d5981d55283ad98fc159de
markharrison <mark.harrison17@nhs.net>
2017-08-07 16:12:34 +0100

    improvements

--- run.sh
+++ run.sh
@@ -7,6 +7,28 @@
 #cd /usr/src/ImportTool
 #python3 import.py -d postgres -l -c postgresql://postgres@localhost/openods --verbose
 
-psql -U postgres -c "CREATE DATABASE openods OWNER postgres;"
+
+MAX_ATTEMPTS=10
+ATTEMPT=0
+IS_CREATED=1
+while [[ ${ATTEMPT} -lt ${MAX_ATTEMPTS} ]]
+do
+   psql -U postgres -c "CREATE DATABASE openods OWNER postgres;"
+   if [[ $? -eq 0 ]]
+   then
+     IS_CREATED=0
+     break
+   fi
+   ATTEMPT=$((ATTEMPT + 1))
+   echo "Unable to create database. ${ATTEMPT}/${MAX_ATTEMPTS}"
+   sleep 5
+done
+
+if [[ ${IS_CREATED} -ne 0 ]]
+then
+    echo "ERROR: Unable to create openods database."
+    exit 1
+fi
+
 python3 ./import.py -d postgres -l -c postgresql://postgres@localhost/openods \
   -x ../ods-data/fullfile.zip -s ../ods-data/ancilliary.zip --verbose
